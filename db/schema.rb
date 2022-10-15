a2d8a401a67f3278aaff27ecffc4fc9d93312d34
tomas vargas <tomasvm01@gmail.com>
2022-10-15 17:47:06 -0500

    Se crean route_id (foreing key) en la tabla de stop stations por favor hacer db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_224541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "near_places", force: :cascade do |t|
@@ -73,6 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
     t.integer "next_station"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "route_id", null: false
+    t.index ["route_id"], name: "index_stop_stations_on_route_id"
   end
 
   create_table "stop_times", force: :cascade do |t|
@@ -96,4 +103,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "near_places", "places"
+  add_foreign_key "stop_stations", "routes"
 end

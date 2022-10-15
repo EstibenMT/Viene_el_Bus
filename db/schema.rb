db8fe04216662ba51e13fa2a4da0c96e9dd1ba1e
tomas vargas <tomasvm01@gmail.com>
2022-10-15 12:40:20 -0500

    se hace migración del modelo de near places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_171820) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_173438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_171820) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "near_places", force: :cascade do |t|
+    t.bigint "place_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_near_places_on_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -82,4 +89,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_171820) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "near_places", "places"
 end

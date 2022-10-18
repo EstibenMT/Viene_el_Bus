27c44cfb3bfec3755a7b3172e40c91845f308bfd
Morthuis Ater <morthuisater@gmail.com>
2022-10-18 17:58:25 -0500

    I added foreing keys to favorites from User and Route models, do not forget runing migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_224937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "route_id", null: false
+    t.index ["route_id"], name: "index_favorites_on_route_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "near_places", force: :cascade do |t|
     t.bigint "place_id", null: false
     t.datetime "created_at", null: false
@@ -54,6 +63,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
     t.text "description"
     t.string "longitude"
     t.string "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -95,5 +111,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_174113) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorites", "routes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "near_places", "places"
 end

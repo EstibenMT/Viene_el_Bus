cad23c7686cf47f84080df6c7c82519b6be1f79f
tomas vargas <tomasvm01@gmail.com>
2022-10-18 19:15:46 -0500

    se actualiza master con cambios en la db a favorites

--- db/schema.rb
+++ db/schema.rb
@@ -69,13 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_224937) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.string "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "routes", force: :cascade do |t|
     t.string "name"
     t.integer "price"

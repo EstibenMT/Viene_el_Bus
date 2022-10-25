694a94498782863da90c16ed4bdc8bd224dfc945
estiben <estibenmontoyataborda@gmail.com>
2022-10-25 17:17:14 -0500

    solucion error en las seed modelo location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_234548) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_221234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_234548) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "near_places", force: :cascade do |t|
